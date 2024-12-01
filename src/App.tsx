c74b572c674176e012638e1a5d6287b6d1c96602
Matias Scherer Lunkes <mslunkes@outlook.com>
2024-12-01 19:38:14 -0300

    resolução do exercicio da aula, styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
+import { ThemeProvider } from 'styled-components'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import GlobalStyle from './global'
+import { Botao } from './global'
+
+import temaLight from './themes/light'
+import temaDark from './themes/dark'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const getStoredTheme = () => {
+    const storedTheme = localStorage.getItem('temaDark')
+    return storedTheme === 'true'
+  }
+
+  const [estadousandoTemaDark, setEstadoUsandoTemaDark] =
+    useState(getStoredTheme)
+
+  function trocatema() {
+    setEstadoUsandoTemaDark(!estadousandoTemaDark)
+  }
+
+  useEffect(() => {
+    localStorage.setItem('temaDark', estadousandoTemaDark.toString())
+  }, [estadousandoTemaDark])
+
   return (
-    <>
+    <ThemeProvider theme={estadousandoTemaDark ? temaDark : temaLight}>
+      <GlobalStyle />
       <Header />
-      <Hero />
+      <Hero trocaTema={trocatema} />
       <div className="container">
+        <Botao onClick={trocatema}> Trocar Tema </Botao>
         <ListaVagas />
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ src/themes/light.ts
@@ -0,0 +1,13 @@
+const temaLight = {
+  corPrincipal: '#a7727d',
+  corSecundaria: '#f9f5e7',
+  corDeFundo: '#fff'
+}
+
+export default temaLight
+
+export interface Theme {
+  corPrincipal: string
+  corSecundaria: string
+  corDeFundo: string
+}
